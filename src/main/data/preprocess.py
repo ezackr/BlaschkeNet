33cc8621f6f520f8117a4ea2cb125e0fbdcd719f
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-03-05 21:16:28 -0800

    removed unnecessary helper methods

--- src/main/data/preprocess.py
+++ src/main/data/preprocess.py
@@ -1,4 +1,3 @@
-import math
 from os import walk
 from os.path import join
 from typing import List
@@ -6,8 +5,6 @@ from typing import List
 import numpy as np
 from pydub import AudioSegment
 from tqdm import tqdm
-
-from src.main.util import root_dir
 
 
 def mp3_to_numpy(path: str) -> np.ndarray:
@@ -34,42 +31,3 @@ def mp3_to_numpy_dir(mp3_dir: str) -> List[np.ndarray]:
             waveform = mp3_to_numpy(abs_path)
             waveforms.append(waveform)
     return waveforms
-
-
-def _nearest_power_of_two(n: int) -> int:
-    """
-    Finds the smallest power of two greater than or equal to n.
-    :param n: an integer
-    :return: the smallest power of two greater than or equal to n
-    """
-    exp = int(math.log2(n))
-    if 2 ** exp == n:
-        # n is a power of two
-        return n
-    else:
-        return 2 ** (exp + 1)
-
-
-def pad_waveforms(waveforms: List[np.ndarray]) -> np.ndarray:
-    """
-    Converts a list of variable-length waveforms into a single array by
-    padding waveforms with zeros.
-    :param waveforms: the original waveforms
-    :return: all waveforms as a single array with padded zeros
-    """
-    max_waveform_length = max([len(waveform) for waveform in waveforms])
-    max_pad_length = _nearest_power_of_two(max_waveform_length)
-    padded_waveforms = np.zeros(shape=(len(waveforms), max_pad_length))
-    for i, waveform in enumerate(waveforms):
-        padded_waveforms[i, :len(waveform)] = waveform
-    return padded_waveforms
-
-
-def main():
-    mp3_dir = join(root_dir, "dataset", "archive", "recordings")
-    waveforms = mp3_to_numpy_dir(mp3_dir)
-    padded_waveforms = pad_waveforms(waveforms)
-
-
-if __name__ == "__main__":
-    main()
