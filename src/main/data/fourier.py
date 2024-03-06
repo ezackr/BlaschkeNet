cfa1a774d0b4125005aceff25cec598c08b8a73a
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-03-05 22:30:16 -0800

    added function for computing fourier coefficients

--- src/main/data/fourier.py
+++ src/main/data/fourier.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import numpy as np
 
 
@@ -11,3 +13,26 @@ def remove_negative_frequencies(f: np.ndarray):
     :return: the waveform without negative frequencies
     """
     return 2 * np.fft.ifft(np.maximum(np.fft.fft(f), 0))
+
+
+def get_fourier_coefficients(
+        f: np.ndarray,
+        degree: int = None,
+        sampling_rate: int = 44100
+) -> List[float]:
+    """
+    Computes the fourier coefficients of a given waveform function f.
+    :param f: a waveform
+    :param degree: the number of terms in the fourier series
+    :param sampling_rate: the sampling rate of the waveform
+    :return: a list of fourier coefficients
+    """
+    # use maximum degree polynomial if none provided
+    if not degree:
+        degree = len(f)
+    time_values = np.arange(0, len(f)) / sampling_rate
+    coefficients = []
+    for i in range(degree + 1):
+        c_i = np.sum(f * np.exp(-1j * 2 * np.pi * i * time_values / len(f))) / len(f)
+        coefficients.append(c_i)
+    return coefficients
