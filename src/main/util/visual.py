0a8df10c1e692bf358aa51d51700de55b6a7e236
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-03-06 13:47:24 -0800

    fixed visualization arguments

--- src/main/util/visual.py
+++ src/main/util/visual.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 def plot_waveform(waveform: np.ndarray, sampling_rate: int = 44100):
-    plt.plot(x=len(waveform) / sampling_rate, y=waveform)
+    plt.plot(np.arange(len(waveform)) / sampling_rate, waveform)
     plt.xlabel("Time (s)")
     plt.ylabel("Amplitude")
     plt.show()
