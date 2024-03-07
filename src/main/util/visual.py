ec425800c14335ebf76b003d4e36ac1da4dc7a58
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-03-06 22:15:23 -0800

    added functions for visualizing the complex plane

--- src/main/util/visual.py
+++ src/main/util/visual.py
@@ -2,8 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_waveform(waveform: np.ndarray, sampling_rate: int = 44100):
+def plot_waveform(waveform: np.ndarray, sampling_rate: int = 44100, title: str = ""):
     plt.plot(np.arange(len(waveform)) / sampling_rate, waveform)
     plt.xlabel("Time (s)")
     plt.ylabel("Amplitude")
+    plt.title(title)
+    plt.show()
+
+
+def plot_complex_plane(complex_values: np.ndarray, title: str = ""):
+    plt.plot(complex_values.real, complex_values.imag)
+    plt.xlabel("Real")
+    plt.ylabel("Imaginary")
+    plt.title(title)
     plt.show()
