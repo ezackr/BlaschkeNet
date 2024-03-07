0d59f2bad7bee3408ed586168238c9f7d348ec75
rob-creon <stevensr@protonmail.ch>
2024-03-07 15:39:23 -0800

    downloader implemented

--- src/main/data/downloader.py
+++ src/main/data/downloader.py
@@ -1,10 +1,16 @@
 from bs4 import BeautifulSoup
+from urllib.request import urlopen, urlretrieve
+from pydub.silence import detect_leading_silence
+from pydub import AudioSegment, silence
+import os
+from pathlib import Path
+from tqdm import tqdm as tqdm
 
 LANG_URL = 'http://accent.gmu.edu/browse_language.php?function=find&language='
 SAMPLE_URL = 'http://accent.gmu.edu/soundtracks/'
 
 
-def fetch_dataset(langs: list[str], target_dir: str, sample_size=5, max_samples=100):
+def fetch_dataset(langs: list[str], target_dir: str, sample_duration=5, max_samples=100):
     """
     Downloads all satisfactory language samples from the speech-accent-archive
     and processes them to remove leading and trailing silence.
@@ -13,21 +19,54 @@ def fetch_dataset(langs: list[str], target_dir: str, sample_size=5, max_samples=
     it is shorter it is thrown out. Samples are saved in mp3 form to the desired
     folder with a subdirectory for each language.
 
-    :param target_dir:
+    :param target_dir: must include trailing /
     :param max_samples: maximum number of samples to take per language
     :param langs: list of names of language to download, all lowercase
-    :param sample_size: length of desired samples in seconds
+    :param sample_duration: length of desired samples in seconds
     :return: dictionary with langs as keys and number of satisfactory
              samples saved as values.
     """
+    dic = {}
+    for lang in langs:
+        num_samples = min(_get_number_of_samples_in_lang(lang), max_samples)
+        sample_index = 0
+
+        Path(f'{target_dir}{lang}').mkdir(parents=True, exist_ok=True)
+        print(f'Downloading samples for {lang}...')
+
+        for i in tqdm(range(num_samples)):
+            download_index = i+1
+            sample_path = f'{target_dir}{lang}/sample{sample_index}.mp3'
+
+            urlretrieve(f'{SAMPLE_URL}{lang}{download_index}.mp3', sample_path)
+
+            audio = AudioSegment.from_mp3(sample_path)
+            audio = _remove_silence(audio)
+
+            if audio.duration_seconds < sample_duration:
+                # Non-satisfactory sample, throw it out
+                os.remove(sample_path)
+            else:
+                audio = audio[:sample_duration*1000]
+                audio.export(sample_path, bitrate='128k')
+                sample_index += 1
+
+        dic[lang] = sample_index
+
+    return dic
 
 
-def _remove_silence(file: str):
+def _remove_silence(audio: AudioSegment):
     """
-    Removes leading and trailing silence from a mp3.
-    :param file: path to mp3
-    :return:
+    Removes leading and trailing silence from an AudioSegment
+    :param audio:
+    :return: cropped AudioSegment
     """
+
+    cropped = audio[detect_leading_silence(audio):]
+    cropped = cropped.reverse()[detect_leading_silence(cropped.reverse()):].reverse()
+
+    return cropped
 
 
 def _get_number_of_samples_in_lang(lang: str):
@@ -36,11 +75,6 @@ def _get_number_of_samples_in_lang(lang: str):
     :param lang:
     :return:
     """
+    soup = BeautifulSoup(urlopen(LANG_URL + lang), 'html.parser')
+    return len(soup.find_all('p'))-1
 
-
-def _download_file(url: str, target_path: str):
-    """
-    Downloads file at url to target_path.
-    :param url:
-    :return:
-    """
--- src/test/data/test_downloader.py
+++ src/test/data/test_downloader.py
@@ -1,10 +1,14 @@
 
-from src.main.data.downloader import _get_number_of_samples_in_lang
+from src.main.data.downloader import _get_number_of_samples_in_lang, fetch_dataset
 
 
 def main():
     assert _get_number_of_samples_in_lang('german') == 45
+    assert _get_number_of_samples_in_lang('bosnian') == 12
+    assert _get_number_of_samples_in_lang('english') == 660
+    fetch_dataset(['english', 'german', 'bengali'], 'download_test/', max_samples=5)
 
 
 if __name__ == '__main__':
     main()
+
